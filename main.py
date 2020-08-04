2f0fc77c08ad8263866e1a9c65618d426301b882
Lnardon <lucaslnardon@gmail.com>
2020-08-04 04:44:13 -0300

    Created Basic Script Functionality

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import shutil
+
+# Gets the folder path to organize
+folderPath = sys.argv[1]
+
+# Goes to specified folder
+os.chdir(folderPath)
+
+# Creates the 4 organizational folders
+os.mkdir('./DownloadedImages')
+os.mkdir('./DownloadedVideos')
+os.mkdir('./DownloadedAudios')
+os.mkdir('./DownloadedMisc')
+
+# # Scans all the files types and move them to the correct folder
+for file in os.listdir(folderPath):
+    if os.path.isdir(folderPath+'/'+file):
+        continue
+    if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".tiff") or file.endswith(".svg"):
+        shutil.move(file, './DownloadedImages')
+    elif file.endswith(".mp4") or file.endswith(".mov") or file.endswith(".avi") or file.endswith(".wmv") or file.endswith(".flv"):
+        shutil.move(file, './DownloadedVideos')
+    elif file.endswith(".mp3") or file.endswith(".wav") or file.endswith(".aiff"):
+        shutil.move(file, './DownloadedAudios')
+    else:
+        shutil.move(file, './DownloadedMisc')
