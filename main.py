25da688fc65c9ba0ecc9f44fc48d1190b6c7ead8
Lnardon <lucaslnardon@gmail.com>
2020-08-04 21:27:49 -0300

    Now the files extensions are stored in a tuple to facilitate the insertion of new formats

--- main.py
+++ main.py
@@ -8,24 +8,46 @@ folderPath = sys.argv[1]
 # Goes to the specified folder
 os.chdir(folderPath)
 
-# Creates the 4 organizational folders
-os.mkdir('./DownloadedImages')
-os.mkdir('./DownloadedVideos')
-os.mkdir('./DownloadedAudios')
-os.mkdir('./DownloadedMisc')
+# Tuple of file formats
+imageFormats = (".png", ".jpg", ".jpeg", ".gif")
+videoFormats = (".mp4", ".mov", ".avi")
+audioFormats = (".mp3", ".wav", ".aiff")
+docsFormats = (".pdf", ".docx", ".pptx", ".txt")
 
-# # Scans all the files types and move them to the correct folder
+# Scans all the files types and move them to the correct folder
 for file in os.listdir(folderPath):
     # If the file is a folder the program will ignore it
     if os.path.isdir(folderPath+'/'+file):
         continue
-    if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".tiff") or file.endswith(".svg"):
+    if file.endswith(imageFormats):
+        try:
+            os.mkdir('./DownloadedImages')
+        except:
+            print("Folder already exists")
         shutil.move(file, './DownloadedImages')
-    elif file.endswith(".mp4") or file.endswith(".mov") or file.endswith(".avi") or file.endswith(".wmv") or file.endswith(".flv"):
+    elif file.endswith(videoFormats):
+        try:
+            os.mkdir('./DownloadedImages')
+        except:
+            print("Folder already exists")
         shutil.move(file, './DownloadedVideos')
-    elif file.endswith(".mp3") or file.endswith(".wav") or file.endswith(".aiff"):
+    elif file.endswith(audioFormats):
+        try:
+            os.mkdir('./DownloadedAudios')
+        except:
+            print("Folder already exists")
         shutil.move(file, './DownloadedAudios')
+    elif file.endswith(docsFormats):
+        try:
+            os.mkdir('./DownloadedDocs')
+        except:
+            print("Folder already exists")
+        shutil.move(file, './DownloadedDocs')
     else:
+        try:
+            os.mkdir('./DownloadedMisc')
+        except:
+            print("Folder already exists")
         shutil.move(file, './DownloadedMisc')
         
 print("CLEANUP COMPLETED !!!")
